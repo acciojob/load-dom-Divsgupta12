e56da923d05de54b62a2115d6dd79d15bedc1ae9
Divya Gupta <117669677+Divsgupta12@users.noreply.github.com>
2023-06-15 15:12:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let body = document.querySelector("body");
+	document.addEventListner("DOMContentLoaded",function () {
+	body.textContent = "DOM load success";
+});
